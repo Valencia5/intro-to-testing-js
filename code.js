24f7785091497ffe25c005d219f73e23ebe3ba1b
Valencia5 <moses.valencia55@gmail.com>
2022-03-23 16:35:36 -0500

    finished

--- code.js
+++ code.js
@@ -3,10 +3,13 @@ function helloWorld() {
     return "Hello, World!";
 }
 
-function sayHello(input){
-if (input === "Alex"){
-return "Hello, Alex!"
-}else {
-    return "Hello"
-} }
-sayHello("Alex")
+function sayHello(input) {
+    if (input === "Alex") {
+        return "Hello, Alex!";
+    } else if (input === "Pat"){
+        return "Hello, Pat!"
+    } else {
+        return "Hello, Jane!";
+    }
+}
+
